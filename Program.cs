1d45c86e9304459a32828d59b45b5874819ed7c3
Dilek Ayşe Gün <58185677+DilekAyse@users.noreply.github.com>
2021-03-11 17:31:06 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,13 +10,13 @@ namespace SeleniumTestTool
         {
             IWebDriver driver = new FirefoxDriver();
             string link = @"https://www.n11.com/";
-            //sitemizi açmak için
+            
             driver.Navigate().GoToUrl(link);
             driver.FindElement(By.ClassName("closeBtn")).Click();
             driver.FindElement(By.ClassName("dn-slide-deny-btn")).Click();
             driver.FindElement(By.Id("searchData")).SendKeys("bilgisayar");
             driver.FindElement(By.ClassName("searchBtn")).Click();
-            //bazı durumlarda class tanımlanmamış olabiliyor o zaman XPath ile yolunu göstermemiz gerekmekte.
+            //bazı durumlarda class tanımlanmamış olabiliyor o zaman XPath ile yolunu belirtmemiz gerekmekte.
             driver.FindElement(By.XPath(".//*[@id='contentListing']/div/div/div[2]/div[5]/a[2]")).Click();
             driver.FindElement(By.XPath(".//*[@id='p-434348820']/div[1]/a")).Click();
             driver.FindElement(By.ClassName("addBasketUnify")).Click();
